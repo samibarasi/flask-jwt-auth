a73d242653c4f9287a86772fc0debfa51e558505
Sami Barasi <sami.barasi@nakedtoast.de>
2021-04-16 18:56:03 +0200

    add register route

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -6,6 +6,8 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 app = Flask(__name__)
 
 app_settings = os.getenv(
@@ -16,3 +18,6 @@ app.config.from_object(app_settings)
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+
+from project.server.auth.views import auth_blueprint
+app.register_blueprint(auth_blueprint)
